e587807d851ef6132482b14207af6477e717c6ac
Lenore Chen <lenore@profitablesoftware.com>
2021-10-07 00:04:43 -0700

    remove shader effect

--- src/world_init.cpp
+++ src/world_init.cpp
@@ -8,7 +8,7 @@ entt::entity createSalmon(RenderSystem* renderer, vec2 pos)
 	motion.angle = 0.f;
 	motion.velocity = { 0.f, 0.f };
 	motion.scale = mesh.original_size * 75.f;
-	motion.scale.x *= -1;
+	motion.scale.x *= 1.5;
 
 	const entt::entity e = registry.create();
 	registry.emplace<Player>(e);
