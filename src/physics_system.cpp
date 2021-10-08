5997359eb0897960aa0f238cbb40a87dafe45883
shaqil10 <shaqilrahemtulla@hotmail.com>
2021-10-07 19:41:48 -0700

    commented out overlap function to fix collisions

--- src/physics_system.cpp
+++ src/physics_system.cpp
@@ -9,73 +9,74 @@ vec2 get_bounding_box(const Motion& motion)
 	return { abs(motion.scale.x), abs(motion.scale.y) };
 }
 
-// This is a SUPER APPROXIMATE check that puts a circle around the bounding boxes and sees
-// if the center point of either object is inside the other's bounding-box-circle. You can
-// surely implement a more accurate detection
+// This is a check that puts a circle around the bounding boxes and sees
+// if the center point of either object is inside the other's bounding-box-circle.
 bool collides(const Motion& motion1, const Motion& motion2)
 {
 	vec2 dp = motion1.position - motion2.position;
 	float dist_squared = dot(dp,dp);
-	const vec2 other_bonding_box = get_bounding_box(motion1) / 2.f;
-	const float other_r_squared = dot(other_bonding_box, other_bonding_box);
-	const vec2 my_bonding_box = get_bounding_box(motion2) / 2.f;
-	const float my_r_squared = dot(my_bonding_box, my_bonding_box);
+	const vec2 other_bounding_box = get_bounding_box(motion1) / 2.f;
+	const float other_r_squared = dot(other_bounding_box, other_bounding_box);
+	const vec2 my_bounding_box = get_bounding_box(motion2) / 2.f;
+	const float my_r_squared = dot(my_bounding_box, my_bounding_box);
 	const float r_squared = max(other_r_squared, my_r_squared);
 	if (dist_squared < r_squared)
 		return true;
 	return false;
 }
 
-void impulseCollisionResolution(Motion& motion, Motion& motion_other) {
+void impulseCollisionResolution(Motion& player_motion, Motion& motion_other) {
 	// Based on math derived here: https://www.randygaul.net/2013/03/27/game-physics-engine-part-1-impulse-resolution/
 	// Using impulse to resolve collisions
-	vec2 norm = glm::normalize(motion_other.position - motion.position); // collision normal
-	vec2 relative_vel = motion_other.velocity - motion.velocity;
+	vec2 norm = glm::normalize(motion_other.position - player_motion.position); // collision normal
+	vec2 relative_vel = motion_other.velocity - player_motion.velocity;
 	if (glm::dot(norm, relative_vel) > 0) // don't calculate this more than once
 		return;
-	float coeff_restitution = min(motion.coeff_rest, motion_other.coeff_rest);
+	float coeff_restitution = min(player_motion.coeff_rest, motion_other.coeff_rest);
 	
 	// calculated based on "VelocityA + Impulse(Direction) / MassA - VelocityB + Impulse(Direction) / MassB = -Restitution(VelocityRelativeAtoB) * Direction"
-	float impulse_magnitude = -(coeff_restitution + 1) * glm::dot(norm, relative_vel) / (1 / motion.mass + 1 / motion_other.mass); 
+	float impulse_magnitude = -(coeff_restitution + 1) * glm::dot(norm, relative_vel) / (1 / player_motion.mass + 1 / motion_other.mass); 
 	vec2 impulse = impulse_magnitude * norm; // impulse along direction of collision norm
 
 	// velocity is changed relative to the mass of objects in the collision
-	motion.velocity = motion.velocity - impulse / motion.mass; 
+	player_motion.velocity = player_motion.velocity - impulse / player_motion.mass; 
 	motion_other.velocity = motion_other.velocity + impulse / motion_other.mass;
 }
 
+// TODO: Optimization needed for overlap handling/clipping, weird behaviour occurring with certain collisions
 void preventCollisionOverlap(entt::entity entity, entt::entity other) {
-	Motion& circle = registry.get<Motion>(entity);
-	Motion& rectangle = registry.get<Motion>(other);
+	Motion& player = registry.get<Motion>(entity);
+	Motion& enemy = registry.get<Motion>(other);
 	const float pen_scaling_factor = 10;
-	vec2 box_c = get_bounding_box(circle);
-	vec2 box_r = get_bounding_box(rectangle);
-	float x_rectangle_bound = rectangle.position[0] - box_r[0] / 2.f;
-	float x_circle_bound = circle.position[0] + box_c[0] / 2.f;
-	float x_penetration = x_circle_bound - x_rectangle_bound;
+	vec2 box_player = get_bounding_box(player);
+	vec2 box_enemy = get_bounding_box(enemy);
+	float x_enemy_bound = enemy.position[0] - box_enemy[0] / 2.f;
+	float x_player_bound = player.position[0] + box_player[0] / 2.f;
+	float x_penetration = x_player_bound - x_enemy_bound;
 	x_penetration /= pen_scaling_factor;
-	float y_rectangle_bound = rectangle.position[1] - box_r[1] / 2.f;
-	float y_circle_bound = circle.position[1] + box_c[1] / 2.f;
+	float y_rectangle_bound = enemy.position[1] - box_enemy[1] / 2.f;
+	float y_circle_bound = player.position[1] + box_player[1] / 2.f;
 	float y_penetration = y_circle_bound - y_rectangle_bound;
 	y_penetration /= pen_scaling_factor;
-	if (circle.velocity[0] < 0) {
-		circle.position[0] = circle.position[0] + x_penetration / 2.f;
-		rectangle.position[0] = rectangle.position[0] - x_penetration / 2.f;
+
+	if (glm::dot(enemy.velocity, player.velocity) > 0) {
+		if (player.velocity[0] < 0) {
+			player.position[0] = player.position[0] + x_penetration / 2.f;
+			enemy.position[0] = enemy.position[0] - x_penetration / 2.f;
+		}
+		else {
+			player.position[0] = player.position[0] - x_penetration / 2.f;
+			enemy.position[0] = enemy.position[0] + x_penetration / 2.f;
+		}
+		if (player.velocity[1] > 0) {
+			player.position[1] = player.position[1] + y_penetration / 2.f;
+			enemy.position[1] = enemy.position[1] - y_penetration / 2.f;
+		}
+		else {
+			player.position[1] = player.position[1] - y_penetration / 2.f;
+			enemy.position[1] = enemy.position[1] + y_penetration / 2.f;
+		}
 	}
-	else {
-		circle.position[0] = circle.position[0] - x_penetration / 2.f;
-		rectangle.position[0] = rectangle.position[0] + x_penetration / 2.f;
-	}
-	if (circle.velocity[1] > 0) {
-		circle.position[1] = circle.position[1] + y_penetration / 2.f;
-		rectangle.position[1] = rectangle.position[1] - y_penetration / 2.f;
-	}
-	else {
-		circle.position[1] = circle.position[1] - y_penetration / 2.f;
-		rectangle.position[1] = rectangle.position[1] + y_penetration / 2.f;
-	}
-	/*printf("x pen: %f\n", x_penetration);
-	printf("y pen: %f\n", y_penetration);*/
 }
 
 void PhysicsSystem::step(float elapsed_ms, float window_width_px, float window_height_px)
@@ -84,18 +85,11 @@ void PhysicsSystem::step(float elapsed_ms, float window_width_px, float window_h
 	// having entities move at different speed based on the machine.
 	for(entt::entity entity: registry.view<Motion>())
 	{
-		// !!! TODO A1: update motion.position based on step_seconds and motion.velocity
 		Motion& motion = registry.get<Motion>(entity);
 		float step_seconds = 1.0f * (elapsed_ms / 1000.f);
 		motion.position[0] += motion.velocity[0] * step_seconds;
 		motion.position[1] += motion.velocity[1] * step_seconds;
-		(void)elapsed_ms; // placeholder to silence unused warning until implemented
 	}
-
-	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	// TODO A3: HANDLE PEBBLE UPDATES HERE
-	// DON'T WORRY ABOUT THIS UNTIL ASSIGNMENT 3
-	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 	// Check for collisions between all moving entities
 	for(entt::entity entity : registry.view<Motion>())
@@ -110,29 +104,17 @@ void PhysicsSystem::step(float elapsed_ms, float window_width_px, float window_h
 			if (collides(motion, motion_other))
 			{
 				impulseCollisionResolution(motion, motion_other);
-				registry.emplace<Collision>(other, entity);
-				preventCollisionOverlap(other, entity);
-				// Create a collisions event
-				// We are abusing the ECS system a bit in that we potentially insert muliple collisions for the same entity
-				//registry.emplace<Collision>(entity, other);
+				registry.emplace<Collision>(entity, other);
+
+				// TODO: Optimization needed for overlap handling/clipping
+				//preventCollisionOverlap(other, entity);
 				
 			}
 		}
 	}
 
-	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	// TODO A2: HANDLE SALMON - WALL collisions HERE
-	// DON'T WORRY ABOUT THIS UNTIL ASSIGNMENT 2
-	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
 	// you may need the following quantities to compute wall positions
 	(float)window_width_px; (float)window_height_px;
-
-	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	// TODO A2: DRAW DEBUG INFO HERE on Salmon mesh collision
-	// DON'T WORRY ABOUT THIS UNTIL ASSIGNMENT 2
-	// You will want to use the createLine from world_init.hpp
-	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 	auto motion_view = registry.view<Motion>();
 	// debugging of bounding boxes
@@ -147,17 +129,6 @@ void PhysicsSystem::step(float elapsed_ms, float window_width_px, float window_h
 			// visualize the radius with two axis-aligned lines
 			const vec2 bonding_box = get_bounding_box(motion_i);
 			float radius = sqrt(dot(bonding_box/2.f, bonding_box/2.f));
-			//vec2 line_scale1 = { motion_i.scale.x / 10, 2*radius };
-			//entt::entity line1 = createLine(motion_i.position, line_scale1);
-			//vec2 line_scale2 = { 2*radius, motion_i.scale.x / 10};
-			//entt::entity line2 = createLine(motion_i.position, line_scale2);
-
-			// !!! TODO A2: implement debugging of bounding boxes and mesh
 		}
 	}
-
-	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	// TODO A3: HANDLE PEBBLE collisions HERE
-	// DON'T WORRY ABOUT THIS UNTIL ASSIGNMENT 3
-	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 }
