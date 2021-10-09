926e295831be250bc207106861bfef9bf418ade3
UBC Student <weiyi017@students.cs.ubc.ca>
2021-10-08 22:46:56 -0700

    update ai_system.hpp
    added #include files (component ,entt)

--- src/ai_system.hpp
+++ src/ai_system.hpp
@@ -3,6 +3,9 @@
 #include <vector>
 
 #include "common.hpp"
+#include "components.hpp"
+
+#include <entt.hpp>
 
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // DON'T WORRY ABOUT THIS CLASS UNTIL ASSIGNMENT 3
@@ -12,4 +15,6 @@ class AISystem
 {
 public:
 	void step(float elapsed_ms);
+
+	
 };
