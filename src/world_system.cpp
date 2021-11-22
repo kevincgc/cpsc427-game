b125b5ba16dc5fc5ed96dd9afd0360d54870bc6f
Andrew <atsai12@gmail.com>
2021-11-21 20:32:58 -0800

    Minor formatting and adding comments

--- src/world_system.cpp
+++ src/world_system.cpp
@@ -325,18 +325,21 @@ bool WorldSystem::step(float elapsed_ms_since_last_update) {
 		// restart the game once the death timer expired
 		if (counter.counter_ms < 0) {
 			registry.remove<DeathTimer>(entity);
+
+			// Set the flag to start a cutscene
 			cutscene_1_frame_0 = true;
-			if (death_count == 2) {
-				cutscene_speaker = cutscene_speaker::SPEAKER_DRONE_LAUGHING;
-			}
-			else if (death_count == 4) {
-				cutscene_speaker = cutscene_speaker::SPEAKER_DRONE_SAD;
-			}
-			else {
-				cutscene_speaker = cutscene_speaker::SPEAKER_MINOTAUR;
-			}
+
+			// Determine which cutscene speaker to display
+			if		(death_count == 2) { cutscene_speaker = cutscene_speaker::SPEAKER_DRONE_LAUGHING; }
+			else if (death_count == 4) { cutscene_speaker = cutscene_speaker::SPEAKER_DRONE_SAD; }
+			else					   { cutscene_speaker = cutscene_speaker::SPEAKER_MINOTAUR; }
+
+			// Set the cutscene to draw
 			cutscene_selection = 100 + death_count;
-			 state = ProgramState::GAME_OVER_DEAD;
+
+			// Set the game state to game over
+			state = ProgramState::GAME_OVER_DEAD;
+
 			return true;
 		}
 	}
